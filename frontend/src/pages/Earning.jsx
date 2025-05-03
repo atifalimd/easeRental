5e98fca944255a4a310a74e90d6c853b5470baa5
atifalimd <atifalimd@gmail.com>
2025-05-03 21:02:35 +0100

    added Hello

--- frontend/src/pages/Earning.jsx
+++ frontend/src/pages/Earning.jsx
@@ -16,6 +16,7 @@ export default function Earnings() {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
+            // hello
           },
         });
 
